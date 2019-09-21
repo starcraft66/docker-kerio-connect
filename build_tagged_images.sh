7f1e686d4b84eb66a9969cb036bcd27dc856717e
Tristan Gosselin-Hane <starcraft66@gmail.com>
2019-09-20 22:53:53 -0400

    Build the master branch using github actions

--- build_tagged_images.sh
+++ build_tagged_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-for tag in $(git tag); do
+IS_LATEST=0
+for tag in $(git tag --sort=taggerdate); do
     git checkout tags/$tag
     echo "Building tag $tag."
     docker build --tag starcraft66/kerio-connect:$tag --tag docker.pkg.github.com/starcraft66/docker-kerio-connect/kerio-connect:$tag .
@@ -8,5 +8,11 @@ for tag in $(git tag); do
     docker push starcraft66/kerio-connect:$tag
     echo "Pushing tag $tag to github packages."
     docker push docker.pkg.github.com/starcraft66/docker-kerio-connect/kerio-connect:$tag
+    if [ x$IS_LATEST == x"0" ]; then
+        # The most recent git tag should be the "latest" docker tag
+        docker push starcraft66/kerio-connect:latest
+        docker push docker.pkg.github.com/starcraft66/docker-kerio-connect/kerio-connect:latest
+    fi
+    IS_LATEST=1
 done
 echo "Done building."
